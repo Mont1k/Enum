844bd9534e8a2600889125e06127b7e19036961a
Mont1k <104359868+Mont1k@users.noreply.github.com>
2022-08-28 23:44:00 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     Scanner sc = new Scanner(System.in);
+               String a = sc.next().toUpperCase();
+               Days days = Days.valueOf(a);
+                switch (days){
+                    case MONDAY -> System.out.println("Дүйшөмбу - Session");
+                    case TUESDAY -> System.out.println("Шейшемби - Practice");
+                    case WEDNESDAY -> System.out.println("Шаршемби - Session");
+                    case THURSDAY -> System.out.println("Бейшемби - Practice");
+                    case FRIDAY -> System.out.println("Жума - Session");
+                    case SATURDAY -> System.out.println("Ишемби - Chill");
+                    case SUNDAY -> System.out.println("Жекшемби - Chill");
+                }
     }
 }
